a5415ce2cd7d77c640ec40b9e41a41ea0638b7c6
zlase <1454596199@qq.com>
2022-06-08 22:20:51 +0800

    [Feature][完成每日一题1037]

---
+++ src/main/java/leetCode/LeetCode_1037.java
@@ -0,0 +1,19 @@
+package leetCode;
+
+/**
+ * @authoer: zlase
+ * @createDate:2022/6/8
+ * @description:
+ */
+public class LeetCode_1037 {
+
+    // tan斜率比较即可，注意不要用除法即可
+    public boolean isBoomerang(int[][] points) {
+        int x1 = points[0][0] - points[1][0];
+        int y1 = points[0][1] - points[1][1];
+        int x2 = points[0][0] - points[2][0];
+        int y2 = points[0][1] - points[2][1];
+        return x1 * y2 != x2 * y1;
+    }
+
+}
