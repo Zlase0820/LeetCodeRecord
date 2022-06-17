3c39434d793c7692d6e5eb2b45b79a7090f66a10
zlase <1454596199@qq.com>
2022-06-17 20:50:47 +0800

    [Feature][完成每日一题1089]

---
+++ src/main/java/leetCode/LeetCode_1089.java
@@ -0,0 +1,43 @@
+package leetCode;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: zlase
+ * @createDate: 2022/6/17
+ * @description:
+ */
+public class LeetCode_1089 {
+    public void duplicateZeros(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length && list.size() <= arr.length; i++) {
+            if (arr[i] != 0) {
+                list.add(arr[i]);
+            } else {
+                list.add(0);
+                list.add(0);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+    }
+
+    @Test
+    public void test1() {
+        int[] nums = {1, 0, 2, 3, 0, 4, 5, 0};
+        int[] result = {1, 0, 0, 2, 3, 0, 0, 4};
+        LeetCode_1089 leetCode_1089 = new LeetCode_1089();
+        leetCode_1089.duplicateZeros(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != result[i]) {
+                System.out.println("error");
+            }
+        }
+    }
+
+
+}
